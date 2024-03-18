24f1b762a3af5e4e1cf5b1efcf10afd44f332428
rtek1000 <rh.silicon@gmail.com>
2024-03-18 13:40:31 -0300

    Update example-04.ino
    Unit KB to kB

--- example-04/example-04.ino
+++ example-04/example-04.ino
@@ -163,7 +163,7 @@ String listFiles(bool ishtml) {
 // source: https://github.com/CelliesProjects/minimalUploadAuthESP32
 String humanReadableSize(const size_t bytes) {
   if (bytes < 1024) return String(bytes) + " B";
-  else if (bytes < (1024 * 1024)) return String(bytes / 1024.0) + " KB";
+  else if (bytes < (1024 * 1024)) return String(bytes / 1024.0) + " kB";
   else if (bytes < (1024 * 1024 * 1024)) return String(bytes / 1024.0 / 1024.0) + " MB";
   else return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
 }
