9cc27efa89694bb9dc76f0c322e93707249d571c
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2024-07-08 00:34:20 +0200

    format and clean code

--- example-06/webserver.ino
+++ example-06/webserver.ino
@@ -43,59 +43,54 @@ void configureWebServer() {
 
   server->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     String logmessage = "Client:" + request->client()->remoteIP().toString() + +" " + request->url();
-
-      logmessage += " Auth: Success";
-      Serial.println(logmessage);
-      request->send_P(200, "text/html", index_html, processor);
+    Serial.println(logmessage);
+    request->send_P(200, "text/html", index_html, processor);
   });
 
   server->on("/reboot", HTTP_GET, [](AsyncWebServerRequest *request) {
     String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
 
-      request->send(200, "text/html", reboot_html);
-      logmessage += " Auth: Success";
-      Serial.println(logmessage);
-      shouldReboot = true;
+    request->send(200, "text/html", reboot_html);
+    Serial.println(logmessage);
+    shouldReboot = true;
   });
 
   server->on("/listfiles", HTTP_GET, [](AsyncWebServerRequest *request) {
     String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
-      logmessage += " Auth: Success";
-      Serial.println(logmessage);
-      request->send(200, "text/plain", listFiles(true));
+    logmessage += " Auth: Success";
+    Serial.println(logmessage);
+    request->send(200, "text/plain", listFiles(true));
   });
 
   server->on("/file", HTTP_GET, [](AsyncWebServerRequest *request) {
     String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
-      if (request->hasParam("name") && request->hasParam("action")) {
-        const char *fileName = request->getParam("name")->value().c_str();
-        const char *fileAction = request->getParam("action")->value().c_str();
+    if (request->hasParam("name") && request->hasParam("action")) {
+      const char *fileName = request->getParam("name")->value().c_str();
+      const char *fileAction = request->getParam("action")->value().c_str();
 
-        logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url() + "?name=" + String(fileName) + "&action=" + String(fileAction);
-
-        if (!FFat.exists(String('/') + fileName)) {
-          Serial.println(logmessage + " ERROR: file does not exist");
-          request->send(400, "text/plain", "ERROR: file does not exist");
-        } else {
-          Serial.println(logmessage + " file exists");
-          if (strcmp(fileAction, "download") == 0) {
-            logmessage += " downloaded";
-            request->send(FFat, String('/') + fileName, "application/octet-stream");
-          } else if (strcmp(fileAction, "delete") == 0) {
-            logmessage += " deleted";
-            FFat.remove(String('/') + fileName);
-            request->send(200, "text/plain", "Deleted File: " + String(fileName));
-          } else {
-            logmessage += " ERROR: invalid action param supplied";
-            request->send(400, "text/plain", "ERROR: invalid action param supplied");
-          }
-          Serial.println(logmessage);
-        }
+      logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url() + "?name=" + String(fileName) + "&action=" + String(fileAction);
+      if (!FFat.exists(String('/') + fileName)) {
+        Serial.println(logmessage + " ERROR: file does not exist");
+        request->send(400, "text/plain", "ERROR: file does not exist");
       } else {
-        request->send(400, "text/plain", "ERROR: name and action params required");
+        Serial.println(logmessage + " file exists");
+        if (strcmp(fileAction, "download") == 0) {
+          logmessage += " downloaded";
+          request->send(FFat, String('/') + fileName, "application/octet-stream");
+        } else if (strcmp(fileAction, "delete") == 0) {
+          logmessage += " deleted";
+          FFat.remove(String('/') + fileName);
+          request->send(200, "text/plain", "Deleted File: " + String(fileName));
+        } else {
+          logmessage += " ERROR: invalid action param supplied";
+          request->send(400, "text/plain", "ERROR: invalid action param supplied");
+        }
+        Serial.println(logmessage);
       }
+    } else {
+      request->send(400, "text/plain", "ERROR: name and action params required");
+    }
   });
-
 }
 
 void notFound(AsyncWebServerRequest *request) {
@@ -106,28 +101,28 @@ void notFound(AsyncWebServerRequest *request) {
 
 // handles uploads to the filserver
 void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
-    String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+  String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+  Serial.println(logmessage);
+
+  if (!index) {
+    logmessage = "Upload Start: " + String(filename);
+    // open the file on first call and store the file handle in the request object
+    request->_tempFile = FFat.open("/" + filename, "w");
     Serial.println(logmessage);
+  }
 
-    if (!index) {
-      logmessage = "Upload Start: " + String(filename);
-      // open the file on first call and store the file handle in the request object
-      request->_tempFile = FFat.open("/" + filename, "w");
-      Serial.println(logmessage);
-    }
+  if (len) {
+    // stream the incoming chunk to the opened file
+    request->_tempFile.write(data, len);
+    logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
+    Serial.println(logmessage);
+  }
 
-    if (len) {
-      // stream the incoming chunk to the opened file
-      request->_tempFile.write(data, len);
-      logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
-      Serial.println(logmessage);
-    }
-
-    if (final) {
-      logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
-      // close the file handle as the upload is now done
-      request->_tempFile.close();
-      Serial.println(logmessage);
-      request->redirect("/");
-    }
+  if (final) {
+    logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
+    // close the file handle as the upload is now done
+    request->_tempFile.close();
+    Serial.println(logmessage);
+    request->redirect("/");
+  }
 }
